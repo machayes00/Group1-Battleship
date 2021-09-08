44d35b0b84c521dfd47585c0672d3b1211b345d5
Edina Harsay <harsay@ku.edu>
2021-09-08 04:35:25 -0500

    Fixed location of added .py file

---
+++ Battleship-G1.py
@@ -0,0 +1,18 @@
+# so far just an empty file
+# Need to modularize and break up to multiple files, probably.
+# I've only ever used multiple program files with C++
+# but I guess it is similar in python??
+
+# Suggested files based on how I learned to break up C++:
+
+'''
+main.py
+executive.py
+gameboard.py
+boat.py
+
+'''
+
+
+
+
