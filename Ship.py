90e2614fc4479f67b07956541c68f24ff4c66fc7
alexarcher721 <89867987+alexarcher721@users.noreply.github.com>
2021-09-10 12:21:21 -0500

    Update Ship.py
    First update to ship.py. Added createbool function(checks if created ship does not overlap with another ship or goes off board) and createship function(add ship to gameboard)

--- Ship.py
+++ Ship.py
@@ -1,1 +1,48 @@
+#assume creation of 8x8 array board filled up with 'o'
+#That array will be the game board
+class ship:
+    def createbool(array,startx,starty,orient,length):
+        check=True
+        start=0
+        if orient == "L":
+            while start <= length:
+                array[startx-start,starty]='x'
+                start=start+1
+        if orient == "R":
+            while start <= length:
+                array[startx+start,starty]='x'
+                start=start+1
+        if orient == "U":
+            while start <= length:
+                array[startx,starty+start] = "x"
+                start=start+1
+        if orient == "D":
+            while start <= length:
+                if array[startx,starty-start] != "o":
+                    check=False
+                start=start+1
+        return(check)
+    def createship(array,startx,starty,orient,length):
+        if createbool(array,startx,starty,orient,length) == True:
+            start=0
+        if orient == "L":
+            while start <= length:
+                if array[startx-start,starty] != "o":
+                    check=False
+                start=start+1
+        if orient == "R":
+            while start <= length:
+                if array[startx+start,starty] != "o":
+                    check=False
+                start=start+1
+        if orient == "U":
+            while start <= length:
+                if array[startx,starty+start] != "o":
+                    check=False
+                start=start+1
+        if orient == "D":
+            while start <= length:
+                if array[startx,starty-start] != "o":
+                    check=False
+                start=start+1
 
