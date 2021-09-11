292524f23c6767caa29f0ad4d43e3670301e7ccd
Mac Hayes <machayes00@gmail.com>
2021-09-10 20:23:42 -0500

    Fixed ship class. Created gameboard file.

--- Ship.py
+++ Ship.py
@@ -1,48 +1,51 @@
 #assume creation of 8x8 array board filled up with 'o'
 #That array will be the game board
 class ship:
-    def createbool(array,startx,starty,orient,length):
-        check=True
-        start=0
-        if orient == "L":
-            while start <= length:
-                array[startx-start,starty]='x'
-                start=start+1
-        if orient == "R":
-            while start <= length:
-                array[startx+start,starty]='x'
-                start=start+1
-        if orient == "U":
-            while start <= length:
-                array[startx,starty+start] = "x"
-                start=start+1
-        if orient == "D":
-            while start <= length:
-                if array[startx,starty-start] != "o":
-                    check=False
-                start=start+1
-        return(check)
-    def createship(array,startx,starty,orient,length):
-        if createbool(array,startx,starty,orient,length) == True:
+    def createbool(self,array,startx,starty,orient,length):
+        start = 0
+        if orient == 'L':
+            while start < length:
+                if array[starty][startx-start] != ' ':
+                    return(False)
+                else:
+                    start=start+1
+        elif orient == 'R':
+            while start < length:
+                if array[starty][startx+start] != ' ':
+                    return(False)
+                else:
+                    start=start+1
+        elif orient == 'U':
+            while start < length:
+                if array[starty-start][startx] != ' ':
+                    return(False)
+                else:
+                    start=start+1
+        if orient == 'D':
+            while start < length:
+                if array[starty+start][startx] != ' ':
+                    return False
+                else:
+                    start=start+1
+        return True
+
+    def createship(self, array, startx, starty, orient, length):
+        if self.createbool(array, startx, starty, orient, length):
             start=0
-        if orient == "L":
-            while start <= length:
-                if array[startx-start,starty] != "o":
-                    check=False
-                start=start+1
-        if orient == "R":
-            while start <= length:
-                if array[startx+start,starty] != "o":
-                    check=False
-                start=start+1
-        if orient == "U":
-            while start <= length:
-                if array[startx,starty+start] != "o":
-                    check=False
-                start=start+1
-        if orient == "D":
-            while start <= length:
-                if array[startx,starty-start] != "o":
-                    check=False
-                start=start+1
+            if orient == 'L':
+                while start < length:
+                    array[starty][startx-start] = 'x'
+                    start=start+1
+            elif orient == 'R':
+                while start < length:
+                    array[starty][startx+start] = 'x'
+                    start=start+1
+            elif orient == 'U':
+                while start < length:
+                    array[starty-start][startx] = 'x'
+                    start=start+1
+            elif orient == 'D':
+                while start < length:
+                    array[starty+start][startx] = 'x'
+                    start=start+1
 
