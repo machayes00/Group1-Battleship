cd6f87d6e9f332558686cbc9f8644e11abfaf283
alexarcher721 <89867987+alexarcher721@users.noreply.github.com>
2021-09-10 21:17:01 -0500

    Update Ship.py
    -Fixed many issues
    -hit function destroys a part of ship
    -sinkcheck function checks if ship is sunk

--- Ship.py
+++ Ship.py
@@ -1,48 +1,72 @@
-#assume creation of 8x8 array board filled up with 'o'
+#assume creation of 10x10 array board filled up with 'o'
 #That array will be the game board
 class ship:
-    def createbool(array,startx,starty,orient,length):
+    def createbool(self,array,startx,starty,orient,length):
         check=True
         start=0
-        if orient == "L":
-            while start <= length:
-                array[startx-start,starty]='x'
+        if orient == 'L':
+            while start < length:
+                if array[(starty*10)+(startx-start)] != "o":
+                    check=False
                 start=start+1
-        if orient == "R":
-            while start <= length:
-                array[startx+start,starty]='x'
+        if orient == 'R':
+            while start < length:
+                if array[(starty*10)+(startx+start)] != "o":
+                    check=False
                 start=start+1
-        if orient == "U":
-            while start <= length:
-                array[startx,starty+start] = "x"
+        if orient == 'U':
+            while start < length:
+                if array[startx+((starty-start)*10)] != "o":
+                    check=False
                 start=start+1
-        if orient == "D":
-            while start <= length:
-                if array[startx,starty-start] != "o":
+        if orient == 'D':
+            while start < length:
+                if array[startx+((starty+start)*10)] != "o":
                     check=False
                 start=start+1
         return(check)
-    def createship(array,startx,starty,orient,length):
-        if createbool(array,startx,starty,orient,length) == True:
+    def createship(self,array,startx,starty,orient,length):
+        if self.createbool(self,array,startx,starty,orient,length) == True:
             start=0
-        if orient == "L":
-            while start <= length:
-                if array[startx-start,starty] != "o":
-                    check=False
-                start=start+1
-        if orient == "R":
-            while start <= length:
-                if array[startx+start,starty] != "o":
-                    check=False
-                start=start+1
-        if orient == "U":
-            while start <= length:
-                if array[startx,starty+start] != "o":
-                    check=False
-                start=start+1
-        if orient == "D":
-            while start <= length:
-                if array[startx,starty-start] != "o":
-                    check=False
-                start=start+1
+            self.shipspots=[]
+            if orient == "L":
+                while start < length:
+                    array[(starty*10)+(startx-start)]='X'
+                    self.shipspots.append([(starty*10)+(startx-start)])
+                    start=start+1
+            if orient == "R":
+                while start < length:
+                    array[(starty*10)+(startx+start)]='X'
+                    self.shipspots.append([(starty*10)+(startx+start)])
+                    start=start+1
+            if orient == "U":
+                while start < length:
+                    array[startx+((starty-start)*10)] = "X"
+                    self.shipspots.append([startx+((starty-start)*10)])
+                    start=start+1
+            if orient == "D":
+                while start < length:
+                    array[startx+((starty+start)*10)] != "X"
+                    self.shipspots.append([startx+((starty+start)*10)])
+                    start=start+1        
+    def hit(self,array,startx,starty):
+        spot=0
+        for x in self.shipspots:
+            if x == array[startx+(starty*10)]:
+                spot=x
+            self.shipspots.remove(x)
+            self.sunk=False
+            if len(self.shipspots) == 0:
+                self.sunk=True
+    def sinkcheck(self):
+        if self.sunk == True:
+            print("Ship is sunk!")
+#s=ship
+#print("Hey")
+#arr=["o" for i in range(100)]
+#s.createship(s,arr,3,1,'R',2)
+#s.hit(s,arr,3,1)
+#s.sinkcheck(s)
+#print(arr)
 
+#Previous lines were used for testing
