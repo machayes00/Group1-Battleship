23284c47418a1bebc997ccaec09ca2ec6330c40f
Edina Harsay <harsay@ku.edu>
2021-09-12 09:50:13 -0500

    Changes in Ship.py as follows: 1. parameter # in if statement of createship was incorrect (because self was listed 2 places); I fixed 2. The "check" variable was unused in createbool, so it should be removed 3. Allow either upper or lower case letter entry for orientation (createbool method) 4. Instead of exception, better to return False, because you don't want program to quit if user enters bad coordinates 4. "array" parameter in hit method was unused but needed to be used; I fixed.

--- Ship.py
+++ Ship.py
@@ -8,42 +8,46 @@ class ship:
     ##documentation for a method
     # @brief checks whether a ship of a certain size, place and orientation fits on a gameboard
     # @pre array is valid
-    # @param array: in place of gameboard
-    # @param startx: x position in array
-    # @param starty: y position in array
-    # @param orient: orientation of ship(left, right, up, down)
+    # @param array: a 10 by 10 array representing a gameboard BUT EDINA thinks this param belongs in Board class
+    # @param startx: x position of start of ship in array
+    # @param starty: y position of start of ship in array
+    # @param orient: orientation of ship(L=left of start, R=right of start, U=up from start, D=down from start)
     # @param length: length of ship
     # @post gives either true or false dependent on whether ship is placeable
     def createbool(self, array, startx, starty, orient, length):
-        check=True
+        # check=True Edina: this variable was not used in the method, so it should be removed
         start=0
         if(startx < 0 or startx > 9):
-            raise Exception("X coordinate is not valid")
+            # raise Exception("X coordinate is not valid") #Edina: I think Exception is if you want program to quit
+            return False # Edina: better to make createbool return False, instead of throw exception
         if(starty < 0 or starty > 9):
-            raise Exception("Y coordinate is not valid")
+            # raise Exception("Y coordinate is not valid")
+            return False
         if(length < 1 or length > 10):
-            raise Exception("Length is not valid")
-        if(orient != 'L' and orient != 'R' and orient != 'U' and orient != 'D'):
-            raise Exception("Orientation is not valid")
-        if orient == 'L':
+            # raise Exception("Length is not valid")
+            return False
+        if(orient != ('L' or 'l') and orient != ('R' or 'r') and orient != ('U' or 'u') and orient != 'D' or 'd'):
+            # raise Exception("Orientation is not valid")
+            return False
+        if orient == ('L' or 'l'):
             while start < length:
                 if array[starty][startx-start] != ' ':
                     return(False)
                 else:
                     start=start+1
-        elif orient == 'R':
+        elif orient == ('R' or 'r'):
             while start < length:
                 if array[starty][startx+start] != ' ':
                     return(False)
                 else:
                     start=start+1
-        elif orient == 'U':
+        elif orient == ('U' or 'u'):
             while start < length:
                 if array[starty-start][startx] != ' ':
                     return(False)
                 else:
                     start=start+1
-        if orient == 'D':
+        if orient == ('D' or 'd'):
             while start < length:
                 if array[starty+start][startx] != ' ':
                     return False
@@ -64,7 +68,7 @@ class ship:
         if self.createbool(array, startx, starty, orient, length): # Edina: deleted self from () because otherwise duplicate
             start=0
             self.shipspots=[]
-            if orient == 'L':
+            if orient == ('L' or 'l'):
                 while start < length:
                     array[starty][startx-start] = 'x'
                     self.shipspots.append([starty][startx-start])
@@ -92,9 +96,9 @@ class ship:
     # @param startx: x position in array
     # @param starty: y position in array
     # @post shipspot is removed as it has been hit   
-    def hit(self,array,startx,starty):
+    def hit(self,array,startx,starty):  # Edina: array parameter was not used in this method; I added to spot
         self.sunk=False
-        spot=[starty][startx]
+        spot=array[starty][startx]
         self.shipspots.remove(spot)
         print("HIT!")
         if len(self.shipspots) == 0:
@@ -107,6 +111,7 @@ class ship:
         if self.sunk == True:
             print("Ship is sunk!")
 
+#Following lines are used for testing
 #s=ship
 #arr=["o" for i in range(100)]
 #s.createship(s,arr,3,1,'R',2)
@@ -115,4 +120,4 @@ class ship:
 #s.sinkcheck(s)
 #for x in range(9):
     #print(arr[(x*10)+0]+" "+arr[(x*10)+1]+" "+arr[(x*10)+2]+" "+arr[(x*10)+3]+" "+arr[(x*10)+4]+" "+arr[(x*10)+5]+" "+arr[(x*10)+6]+" "+arr[(x*10)+7]+" "+arr[(x*10)+8]+" "+arr[(x*10)+9])
-#Previous lines were used for testing
+
