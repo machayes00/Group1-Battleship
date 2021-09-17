0ff3dafde44ef8abe68ec50a5ef9b4074d4b3199
Edina Harsay <harsay@ku.edu>
2021-09-17 04:19:01 -0500

    Clarified Battleship-G1, fixed setup() split 1. Added more clarification comments of what executive methods do, to make clear that setup() can set up the board for each player. (Also, fixed this, so not split into 2 methods.) This also fixed each player being asked number of ships (it only needs to be asked once, before the 2 payer's boards asre set up).
    2. Moved Player1 and Player2 string variables to the playGame method because that is
    where they will be used, probably. Can move out if they need to have global scope.
    They seem like they would be useful but it depends on how playGame() is designed.
    
    3. Fixed parameters for playGame (the inputs are the two Board objects after
    modification by the setup method.
    
    4. Did not alter the printMenu method but wrote a better explanation for what this
    is needed for. Most important is letting player choose between proceeding with the
    game and taking a shot, or quitting, at every single turn. I mostly liked it as is,
    especially that it just gives the option to print instructions in case needed, rather
    than printing them by default.
    
    5. I removed the #'s that were added to my print statements and undid their removal,
    since the welocme statement would only need to be printed a single time at the start
    of the game, and do not need to be in a printMenu that is called repeatedly between
    player turns. I also had to put them back since number of ships only needs to be asked
    a single time for a game, so the print statements were correct.
    
    A minor fix:
    Python convention (and I think an option that Doxygen recognizes, to help make the
    documentation) is to have """explanation""" (or three single quotes on each side)
    beneath the method declaration line, so I moved them back there because some had been
    moved beneath definition bloc. For now some of these are temporary, to aid our work,
    but probably nice to leave clear explanation for doxygen (or Sphinx; maybe that will work too).
    Probably do not delete the temporary explanations 'till we are done, to help everyone
    understand the goals of the method, and what needs to be fixed or added to the method.
    But do edit these, as needed. Just don't remove, at least not yet.

--- Battleship-G1.py
+++ Battleship-G1.py
@@ -1,30 +1,50 @@
 # Import class methods needed for the program
 from Board import *
 
+'''
+Battleship-G1.py interacts with players to obtain values for the parameters
+for methods that set up the game boards, and then it interacts with players
+to obtain shooting coordinates while playing the game. It modifies the game 
+boards accoding to payer inputs.
 
-def setup(board, numberShips): # parameter is a Board object
-    """
-    Need to transpot the createBool code lines from Alex & Mac's
-    Ship class here. I was thinking of getting rid of the exceptions
-    but actually they are fine in try-catch blocks, but I have to 
-    figure out how to do that in Python.
-    Also, in addition to the code from createBool, need more checks using
+The program first obtains the number of ships (same for each player), then 
+sets up a board for each player, calling the setup() method for each player. 
+Once setup completes, the program calls the playGame() method, which interacts 
+with players while they are taking shots trying to hit ships on the opponent's
+board.
+
+The top part of this file lists all the methods, and this is followed by the
+program that sets up and plays the game.
+'''
+choice = 0 # variable that allows quitting the game
+
+##  Documentation for setup method
+#   @brief Interacts with player to set up a board with ships for a player
+#   @param board is a Board object for a player
+#   @param numberShips is an int, same for 2 players
+#   @pre board object must have correct properties; numberShips must be 
+#       in the proper range, 1 to 6
+#   @post the input board object is modified according to user input
+def setup(board, numberShips): 
+    """!
+    In addition to the code from createBool, need to add more checks using
     the waterGrid Board property which this method should update
     with ship locations.
     """
 
-    print('Please enter your ship(s) for your board:\n')
-
     for i in range(numberShips):
-
+        
         # meke this a try-catch block to check acceptable arguments
         startCol = input("\nWhat is the starting column of your ship?\n")
         startRow = input("\nWhat is the starting row of your ship?\n")
         print()
 
-        # I like the idea from Alice&George planning file, to use a
+        # EDIT NEEDED: modify the startCol and starRow variables, after user
+        # input, so that they are indices for 2D waterGrid array.
+        # I like the idea from Alice & George planning file, to use a
         # dictionary to covert coordidantes to index numbers for 
-        # waterGrid 2D array.
+        # waterGrid 2D array. Until we have that, pass in 2D index.
+
 
         print('What is the orientation of this ship? Enter\n')
         print('"L" for left of start (horizontal ship)\n')
@@ -32,64 +52,132 @@ def setup(board, numberShips): # parameter is a Board object
         print('"U" for up from start (vertical ship)\n')
         print('"D" for down from start (vertical ship)\n')
 
-        # try catch block, using code from Alex's original Ship.py
-        # createBool. But we don't need it to be a bool. Also, allow both
-        # upper case and lower case entry.
+        # try catch block, using code from Alex's and Mac's original Ship.py
+        # createBool. I fixed that to allow both upper case and lower case entry.
+
         orient = input()
 
-        board.createShip(startCol, startRow, orient, numberShips) 
-        # enter correct arguments, icluding coversion for start coordinates.
+        # Enters correct arguments, icluding conversion for start coordinates.
         # This will update waterGrid for the next iteration of the for loop.
-
-def playGame():
+        board.createShip(startCol, startRow, orient, numberShips) 
+        
+##  Documentation for playGame method
+#   @brief interacts with both players, and takes their inputs for shooting coordinates
+#   @param boardPlayer1 is a Board object bodified by the setup() method
+#   @param boardPlayer2 is a Board object bodified by the setup() method
+#   @pre appropriate Board objects must be passed in, after setup() modification
+#   @post 
+def playGame(boardPlayer1, boardPlayer2):
     """
     This is the method for asking players to enter the coordinates
-    for shooting at ships, and then calling the board.chedkShots method and 
+    for shooting at ships, and then calling the board.checkShots method and 
     the board.Score method.
     If this gets too complex, possibly make it a separate class,
     with propertyies that include Player objects(?) But my suggestion is
     to at least start it out in this file, in order to start simple and be able
     to test an early form of the game, the minimal things.
     """
+    # not sure these variables are needed; depends on how playGame is designed
+    player1 = "Player 1 "
+    player2 = "Player 2 "
+
+    # Print menu (see next method) will print between each shoot
+    printMenu()
     pass
 
+#G: made a simple little menu, doesn't do anything right now. Used a while loop 
+# instead of a do-while because do-while loops don't exist in python
+# Edina: did not change anything at all in George's print menu. I like how the instructions are not
+# printed unless a player asks to see them. But what I was thinking is to have a menu that prints 
+# options repeatedly while the game is played. The idea is to have this method
+# be called by the playGame() method between each turn. Something simple. We could also
+# add a menu option to print boards but maybe a better idea is to have them print automatically
+# each time a new turn is taken. So, something like this presented by playGame each time a new
+# turn is taken:
+
+# print("\nPlese select an option:\n(1) Take a shot\n (2) Read rules\n(3) Quit game\n ")
+
+# Maybe printMenu should first print the player's own board, then the menu options, and if 
+# player selects to take s shot, pirnt the opponent's board.
+
+# But the menu shoud be short since it is presented repeatedly for each player at ever single turn.
 
 def printMenu():
-
-    """
+    """!
     Print menu items for users, like option to print boards (or make
     that automatic as part of the setup?)  Also, option to quit, etc.
     """
-    pass
+    choice = 0 
+    while (choice != 3):
+        print("\nWelcome to Battleship! Select option 1 to start the game!\n1)Play Game\n2)Rules \n3)Quit")
+        choice = int(input())
+        if(choice == 1):
+            playGame(boardPlayer1, boardPlayer2)
+            print("Would you like to play again?")
 
-# probably the code below should be in a while block, do while players
-# don't want to quit
+        elif(choice == 2):
+            print("-----------------------------------------------------------------------------------------------------------------------------------------------Rules of Battleship-----------------------------------------------------------------------------------------------------------------------------------------------\n")
+            print("Overview:\nBattleship is a two player game where both players secretly place 1 to 6 ships on a 9x10 grid. Taking turns each player announces where on the opponents grid they wish to fire. The opponent must announce whether or not one of the ships was hit. The first player to sink all of the oponents ships wins\n ")
+            print("1)Ship size will be dependent on number of ships chosen. If one ship is chosen each player will be given a 1x1 ship . If two ships are chosen each player will be given a 1x1 and a 1x2 ship and so on.\n")
+            print("2)After the ships have been chosen, players will be able to place and orient their ships, you may place your ship anywhere within the board and orient it up, down, left or right. You may not orient it diagonally or intersect another ship.\n")
+            print("3)Taking turns, the users pick a space on the opponent's board to fire at,each shot must be updated to indicate a hit or miss.\n")
+            print("4)Once a ship has been hit in every space it occupies, it is sunk.\n")
+            print(
+                "5)As the great Colonel Sanders once said \"I'm too drunk to taste this fried chicken. \"\n ")
+        elif(choice == 3):
+            print("\nGoodbye...")
+        else:
+            print("Sorry invalid choice please pick again.\n")
 
-print('\n *** WELCOME TO BATTLESHIP!! ***\n')
-print()
 
-print('How many ships per player for this game?\n')
-print('Ener a number from 1 to 6:\n')
-# try-catch block or if-else to confirm good entry
-numberShips = int(input())
+# Ths next part starts the program.
+quit = 0 # variable for giving option to quit game or play again, once a game is over
 
-# Create a board object for player 1
-boardPlayer1 = Board()
+while quit == "n":
 
-print('\nReady to set up the board for Player 1!\n')
+    print('\n *** WELCOME TO BATTLESHIP!! ***\n')
+    print()
+    # Maybe add instructions from George's printMenu here. But our Project 1 instructions
+    # stated that the game should be "obvious" and not need much instrucion. So maybe 
+    # it is better if the short prompts for user input, plus feedback to user, will let 
+    # the user understand the game, without printing lengthy instrucitons.
 
-setup(boardPlayer1, numberShips)
+    print('How many ships per player for this game?\n')
+    print('Ener a number from 1 to 6:\n')
+    # try-catch block or if-else to confirm good entry
+    numberShips = int(input())
 
-# Create a board object for player 2
-boardPlayer2 = Board()
+    # Create a board object for player 1
+    boardPlayer1 = Board()
 
-print('\nReady to set up the board for Player 2!\n')
+    print('\nReady to set up the board for Player 1!\n')
 
-setup(boardPlayer2, numberShips)
+    # This step runs the setup method for Player 1. The method modifies 
+    # the waterGrid 2D array of boardPlayer1.
+    setup(boardPlayer1, numberShips)
 
-playGame()
+    # Create a board object for player 2
+    boardPlayer2 = Board()
 
-printMenu()
+    print('\nReady to set up the board for Player 2!\n')
+
+    # This step runs the setup method for Player 2. The method modifies
+    # the waterGrid 2D array of boardPlayer2.
+    setup(boardPlayer2, numberShips)
+
+    # This now starts the shooting steps, printing printMenu() between each player's shot
+    playGame(boardPlayer1, boardPlayer2)
+
+    # Once playGame method ends, give players the option to play again rather than exit program.
+    print("\nWould you like to play another game?\n")
+    print('Enter "Y" for yes, "N" for no:\n')
+    choice = input()
+    if input == "Y" or "y":
+        quit = 1
+
+
+
+
 
 
 
