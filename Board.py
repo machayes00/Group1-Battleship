a036b4d97a16b708245e87da13cde3ad46ed3e33
Alice Chen <zchen@ku.edu>
2021-09-19 20:07:04 -0500

    Added "MISS!" message for missed shots.

--- Board.py
+++ Board.py
@@ -2,13 +2,13 @@ class Board:
 
     def __init__(self):
         """
-        This constructs the full board, initialized to be empty of ships. 
-        It contains a 2D self.waterGrid, waterGrid. Locations marked as '0' are water, 
-        and these will be changed to S wherever there is a ship, and to * to 
+        This constructs the full board, initialized to be empty of ships.
+        It contains a 2D self.waterGrid, waterGrid. Locations marked as '0' are water,
+        and these will be changed to S wherever there is a ship, and to * to
         mark locations of hits. The S and * locations are obtained from two lists,
         shipSpots and shots, both of which are initialized to be empty.
         """
-        
+
         self.shipObjects = [] # this is a list of ship objects. They will be checked to determine which ship is hit, and update ship coord, sunk variables
         self.shiplengths=[]
         self.waterGrid = [['O' for col in range(10)] for row in range(9)] # initialize board to be all 'O'
@@ -20,7 +20,7 @@ class Board:
     def printBoard(self):
         """
         This method prints the waterGrid self.waterGrid with a border to mark coordinates
-        (A-J for columns and 1-9 for rows). 
+        (A-J for columns and 1-9 for rows).
         This printed board shows all the ships.
         """
         topOfBoard = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
@@ -36,7 +36,7 @@ class Board:
             for col in range(10):
                 print(self.waterGrid[row][col], " ", end = "")
             print()
-        
+
     ##documentation for a method
     # @brief checks if ship placement would be valid
     # @param startx: x position in self.waterGrid
@@ -59,6 +59,14 @@ class Board:
             for col in range(10):
                 print(self.oppGrid[row][col], " ", end = "")
             print()
+
+    def checkShipOverlap(self, x, y, len):
+        for i in self.waterGrid:
+            for j in i:
+                if j != 'O':
+                    return False
+        return True
+
 
     def isShipValid(self, orient, startx, starty, length):
         start = 0
@@ -93,7 +101,7 @@ class Board:
     #       (L=left of start, R=right of start, U=up from start, D=down from start)
     # @param length: length of ship
     # @post none
-    def createShip(self, startx, starty, orient, length,shipnumber): 
+    def createShip(self, startx, starty, orient, length,shipnumber):
         start = 0
         shipcoords=[]
         self.shiplengths.append(length)
@@ -134,13 +142,15 @@ class Board:
                     self.oppGrid[y][x] = "x"
                     self.spots=self.spots-1
                     self.shiplengths[z]=self.shiplengths[z]-1
-                    print("HIT!")
+                    print("\nHIT!\n")
                     if self.shiplengths[z] == 0:
                         print("Ship is sunk!")
                         self.points=self.points-1
+                else:
+                    print("\nMISS!\n")
         if temp == self.spots:
             self.oppGrid[y][x] = "m"
-            
+
     def score(self,opp):
         print("Player 1 Ships Remaining:"+str(self.points))
         print("Player 2 Ships Remaining:"+str(opp.points))
